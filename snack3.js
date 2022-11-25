f28a5c7b84b49a630215f2abfe23f3e50e7eff56
VincenzoRuggiero <vincenzoru@live.it>
2022-11-25 17:22:52 +0100

    Snack 3

--- snack3.js
+++ snack3.js
@@ -1,9 +1,16 @@
-const array = [3, 4, 7, 10, 2, 5, 9, 8, 1, 6];
+const array = [3, 4, 7, 10, 2, 5, 9, 8, 2, 6];
 
 let sum = 0;
-
+let max = 0;
 for (let i = 0; i < 10; i++) {
   sum += array[i];
+
+  if (array[i] > max) {
+    max = array[i];
+  }
 }
 
-console.log(sum);
+let media = sum / 10;
+
+console.log(max);
+console.log(`${sum} ${media}`);
